e9d068000c5ca658fc95fec23f1c2889f7d5bc53
Divyansh <divyanshburman363@gmail.com>
2026-04-04 19:53:01 +0530

    deployment setup

--- portfolio_backend/portfolio_backend/settings.py
+++ portfolio_backend/portfolio_backend/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t4-=txy&@dz^twbo0)%06$s18z*q*y38&nxld5s6yx9bfd1tt#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS =  os.getenv("ALLOWED_HOSTS", "*").split(",")
 
 
 # Application definition
