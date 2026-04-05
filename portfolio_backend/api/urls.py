b66dc672bc1c53d1d2e61f6a7d05789e607384e2
Divyansh <divyanshburman363@gmail.com>
2026-04-05 11:19:13 +0530

    fix urls

--- portfolio_backend/api/urls.py
+++ portfolio_backend/api/urls.py
@@ -1,5 +1,12 @@
+from django.urls import path
+from . import views
 from rest_framework.routers import DefaultRouter
 from .views import ProjectViewSet, ContactViewSet
+
+urlpatterns = [
+    path('projects/', views.project_list),
+    path('contact/', views.contact_create),
+]
 
 router = DefaultRouter()
 router.register('projects', ProjectViewSet)
--- portfolio_backend/api/views.py
+++ portfolio_backend/api/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Project, Contact
 from .serializers import ProjectSerializer, ContactSerializer
 
@@ -11,3 +13,12 @@ class ProjectViewSet(viewsets.ModelViewSet):
 class ContactViewSet(viewsets.ModelViewSet):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
+
+api_view(['GET'])
+def project_list(request):
+    return Response({"message": "Projects working ✅"})
+
+
+@api_view(['POST'])
+def contact_create(request):
+    return Response({"message": "Contact working ✅"})
