2e3daadb829b0619155da13fbc9c1546d75f5cb0
Divyansh <divyanshburman363@gmail.com>
2026-04-06 10:32:41 +0530

    fix api urls and views

--- portfolio_backend/api/urls.py
+++ portfolio_backend/api/urls.py
@@ -1,16 +1,28 @@
+# from django.urls import path, include
+# from rest_framework.routers import DefaultRouter
+# from .views import ProjectViewSet, ContactViewSet, project_list, contact_create
+
+# router = DefaultRouter()
+# router.register('projects', ProjectViewSet)
+# router.register('contact', ContactViewSet)
+
+# urlpatterns = [
+#     # custom test APIs
+#     path('projects-test/', project_list),
+#     path('contact-test/', contact_create),
+
+#     # main API routes
+#     path('', include(router.urls)),
+# ]
+
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProjectViewSet, ContactViewSet, project_list, contact_create
+from .views import ProjectViewSet, ContactViewSet
 
 router = DefaultRouter()
 router.register('projects', ProjectViewSet)
 router.register('contact', ContactViewSet)
 
 urlpatterns = [
-    # custom test APIs
-    path('projects-test/', project_list),
-    path('contact-test/', contact_create),
-
-    # main API routes
     path('', include(router.urls)),
 ]
